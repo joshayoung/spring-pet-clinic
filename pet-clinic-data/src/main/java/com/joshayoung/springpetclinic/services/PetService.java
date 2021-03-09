1a4a04bab8cd4a3489ded9eeda620472779546d4
Josh Young <j22cal@gmail.com>
2021-03-09 12:46:05 -0600

    Add interfaces

---
+++ pet-clinic-data/src/main/java/com/joshayoung/springpetclinic/services/PetService.java
@@ -0,0 +1,11 @@
+package com.joshayoung.springpetclinic.services;
+
+import com.joshayoung.springpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
