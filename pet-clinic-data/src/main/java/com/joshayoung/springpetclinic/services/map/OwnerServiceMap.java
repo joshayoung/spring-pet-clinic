1d7799ef15e7406d429893075ceb3b5e50b709de
Josh Young <j22cal@gmail.com>
2021-03-11 12:18:07 -0600

    Add map services

---
+++ pet-clinic-data/src/main/java/com/joshayoung/springpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,34 @@
+package com.joshayoung.springpetclinic.services.map;
+
+import com.joshayoung.springpetclinic.model.Owner;
+import com.joshayoung.springpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
